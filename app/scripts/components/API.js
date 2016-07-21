e11d7d8999812c32d1d00f0f8636abd44b795a90
Eric Cecchi <eric.cecchi@gmail.com>
2016-07-21 10:41:51 -0500

    Add support for images and videos

--- app/scripts/components/API.js
+++ app/scripts/components/API.js
@@ -1,5 +1,13 @@
 import ajax from 'axios';
 
+function csvToArray(string) {
+    return string ? string.split(',') : [];
+}
+
+function getYoutubeId(videoUrl) {
+    return videoUrl && (videoUrl.split('v=')[1] || false);
+}
+
 export function getOpportunities() {
     return new Promise((resolve) => {
         ajax.get('https://spreadsheets.google.com/feeds/list/1hetYu_LEuuim1efw0aFxv6lOYLjhU8-e7wkIZHupW_o/1/public/full?alt=json')
@@ -7,8 +15,6 @@ export function getOpportunities() {
                 const raw = response.data.feed.entry;
                 const opportunities = raw
                     .map(opportunity => {
-                        const categoriesString = opportunity['gsx$categories']['$t'];
-                        const categories = categoriesString ? categoriesString.split(',') : [];
                         return {
                             id: opportunity['gsx$id']['$t'],
                             title: opportunity['gsx$title']['$t'],
@@ -16,12 +22,14 @@ export function getOpportunities() {
                             description: opportunity['gsx$description']['$t'],
                             location: opportunity['gsx$location']['$t'],
                             time: opportunity['gsx$time']['$t'],
-                            categories,
+                            categories: csvToArray(opportunity['gsx$categories']['$t']),
                             family: !!opportunity['gsx$family']['$t'],
                             group: !!opportunity['gsx$group']['$t'],
                             individual: !!opportunity['gsx$individual']['$t'],
                             ongoing: !!opportunity['gsx$ongoing']['$t'],
-                            oneTime: !!opportunity['gsx$one-time']['$t']
+                            oneTime: !!opportunity['gsx$one-time']['$t'],
+                            videoId: getYoutubeId(opportunity['gsx$video']['$t']),
+                            imageUrl: opportunity['gsx$image']['$t'] || 'https://www.placecage.com/600/400'
                         }
                     })
                     .filter((opportunity) => !!opportunity.title);
