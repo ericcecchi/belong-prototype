4da47b6a5a04bc2976ddb8c132853e6a2963d821
Eric Cecchi <eric.cecchi@gmail.com>
2016-07-23 15:19:03 -0500

    Change 'A Home' to 'Homes'

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -12,7 +12,7 @@ const Filters = {
 };
 
 const Callouts = {
-    'A Home': 'On any given night, there are over 43 million children who sleep with no roof over their heads. What if we could provide resources, hope, and a place to belong?',
+    'Homes': 'On any given night, there are over 43 million children who sleep with no roof over their heads. What if we could provide resources, hope, and a place to belong?',
     'Health': 'More than 25% of our nation suffers from mental illness alone. Is there a way we could bring healing, hope, and belonging to those without health?',
     'Money': 'While progress is being made, there are still close to 800 million in our world who are chronically undernourished. Join those providing basic needs for the under-resourced.',
     'Freedom': 'There are an estimated 4.5 million people trapped in forced sexual exploitation globally. Join the movement to bring freedom and hope to people who are enslaved or imprisoned today.',
@@ -133,7 +133,7 @@ export default class App extends Component {
                             <option value="Family">without family</option>
                             <option value="Health">without health</option>
                             <option value="Freedom">without freedom</option>
-                            <option value="A Home">without homes</option>
+                            <option value="Homes">without homes</option>
                             <option value="Money">without money</option>
                         </select>
                         <span> near </span>
